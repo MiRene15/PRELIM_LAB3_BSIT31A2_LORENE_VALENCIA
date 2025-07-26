5e8670551208cc0977663456f81316a20034364d
STUDENT <STUDENT@DESKTOP-5L8Q2MB>
2025-07-26 11:33:05 +0800

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,6 @@
+﻿namespace PRELIM_LAB3_BSIT31A2_LORENE_VALENCIA.Models
+{
+    public class Customer
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PRELIM_LAB3_BSIT31A2_LORENE_VALENCIA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
